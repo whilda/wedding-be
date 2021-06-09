a646fbe4bc12d2c242680479dde7fb27ec433b1c
Whilda Chaq <whildachaq@gmail.com>
2021-06-09 21:18:26 +0900

    Wedding Template + little edit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dina Puji Lestari' });
+  /* res.render('index', { title: 'Dina Puji Lestari' }); */
+   res.sendFile(path.join(__dirname+'/../public/index.html'));
 });
 
 module.exports = router;
