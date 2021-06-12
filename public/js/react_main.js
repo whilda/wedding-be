dc4b80818e3dfb0e51053ae7c84c56eb7152b685
Whilda Chaq <whildachaq@gmail.com>
2021-06-12 22:25:50 +0900

    Edit groom picture and background header

---
+++ public/js/react_main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
+
+const jsxContainer = <h1>Hello, world!</h1>;
+const domContainer2 = document.querySelector('#jsx-example');
+ReactDOM.render(jsxContainer, domContainer2);
