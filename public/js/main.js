40b7a63abedbcd138dce187984d04c5d40558641
Whilda Chaq <whildachaq@gmail.com>
2021-12-25 10:29:37 +0700

    Micro settings

--- public/js/main.js
+++ public/js/main.js
@@ -274,10 +274,10 @@
 		// + minutes + "Minutes " + seconds + "Seconds ";
 
 		// Display the result in an element with id="demo"
-		document.getElementById("days").innerHTML = days +" <small>days</small>";
-		document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
-		document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
-		document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
+		document.getElementById("days").innerHTML = days +" <small>Hari</small>";
+		document.getElementById("hours").innerHTML = hours + " <small>Jam</small> ";
+		document.getElementById("minutes").innerHTML = minutes + " <small>Menit</small> ";
+		document.getElementById("seconds").innerHTML = seconds + " <small>Detik</small> ";
 
 		// If the count down is finished, write some text
 		if (distance < 0) {
